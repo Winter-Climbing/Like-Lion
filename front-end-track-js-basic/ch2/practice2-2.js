77fbef1f2d9d9c4b6d30664f6bae59e04b0add8a
Winter-Climbing <97828478+Winter-Climbing@users.noreply.github.com>
2022-06-01 19:42:17 +0900

    FE track js ch-2 complete
    like lion lecture

--- front-end-track-js-basic/ch2/practice2-2.js
+++ front-end-track-js-basic/ch2/practice2-2.js
@@ -4,24 +4,24 @@
 
 
 
-let num = 0;
+// let num = 0;
 
-document.querySelector('#plus').addEventListener('click', function () {
-  num++;
-  document.querySelector('#num').innerHTML = num;
-});
-
-document.querySelector('#minus').addEventListener('click', function () {
-  num--;
-  document.querySelector('#num').innerHTML = num;
-});
-
-
-
-// document.querySelector('.bar').addEventListener('click', function () {
-//   document.querySelector('.bar').innerHTML = '눌렸어!';
-//   document.querySelector('.newBar').classList.toggle('show');
+// document.querySelector('#plus').addEventListener('click', function () {
+//   num++;
+//   document.querySelector('#num').innerHTML = num;
 // });
+
+// document.querySelector('#minus').addEventListener('click', function () {
+//   num--;
+//   document.querySelector('#num').innerHTML = num;
+// });
+
+
+
+document.querySelector('.bar').addEventListener('click', function () {
+  document.querySelector('.bar').innerHTML = '눌렸어!';
+  document.querySelector('.newBar').classList.toggle('show');
+});
 
 
 
